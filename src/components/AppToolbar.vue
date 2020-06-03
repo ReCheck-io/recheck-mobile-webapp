0a3ff9fc6fd6ef825ea1fd3ba696a2f937ed2345
lestad <j.v.gospodinov@gmail.com>
2020-06-03 22:46:38 +0300

    maluk comit

--- src/components/AppToolbar.vue
+++ src/components/AppToolbar.vue
@@ -6,7 +6,7 @@
       @mouseover="hover = true"
       @mouseleave="hover = false"
       :class="{ active: hover }" @click="atHome"
-      >Recheck Mobile</v-toolbar-title>
+      >Recheck Mobile Maluk Comit</v-toolbar-title>
       <v-spacer></v-spacer>
       <router-link to="/">
         <v-btn flat icon>
